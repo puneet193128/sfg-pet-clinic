d894a295d73789d871befb339bd2a356892a1616
Puneet Srivastva <psrivas@amazon.com>
2019-01-24 15:03:18 -0800

    refactoring to multimodule project. closes #1

---
+++ pet-clinic-web/src/test/java/com/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,12 @@
+package com.springframework.sfgpetclinic;
+
+@org.junit.runner.RunWith(org.springframework.test.context.junit4.SpringRunner.class)
+@org.springframework.boot.test.context.SpringBootTest
+public class SfgPetClinicApplicationTests {
+
+    @org.junit.Test
+    public void contextLoads() {
+    }
+
+}
+
