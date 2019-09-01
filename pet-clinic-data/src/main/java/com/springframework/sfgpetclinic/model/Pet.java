be8283b8fc1368d19f9f025d05877200f73b3715
Puneet Srivastva <psrivas@amazon.com>
2019-09-01 15:41:08 -0700

    Added Pet and contact info to owners on startup . Closes #33

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Pet.java
@@ -2,10 +2,18 @@ package com.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 public class Pet extends BaseEntity{
-
+    private String name;
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public PetType getPetType() {
         return petType;
@@ -23,11 +31,11 @@ public class Pet extends BaseEntity{
         this.owner = owner;
     }
 
-    public java.time.LocalDate getBirthDate() {
+    public LocalDate getBirthDate() {
         return birthDate;
     }
 
-    public void setBirthDate(java.time.LocalDate birthDate) {
+    public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
 }
--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.springframework.sfgpetclinic.bootstrap;
 
 import com.springframework.sfgpetclinic.model.Owner;
+import com.springframework.sfgpetclinic.model.Pet;
 import com.springframework.sfgpetclinic.model.PetType;
 import com.springframework.sfgpetclinic.model.Vet;
 import com.springframework.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.springframework.sfgpetclinic.services.PetTypeService;
 import com.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -32,19 +35,39 @@ public class DataLoader implements CommandLineRunner {
         PetType savedDogPetType = petTypeService.save(dog);
 
         PetType cat = new PetType();
-        dog.setName("Cat");
+        cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress(" Test address of owner1");
+        owner1.setCity("Bellevue");
+        owner1.setTelephone("123456789");
+
+        Pet puneetPet = new Pet();
+        puneetPet.setOwner(owner1);
+        puneetPet.setBirthDate(LocalDate.now());
+        puneetPet.setName("Barfi");
+        puneetPet.setPetType(savedDogPetType);
+        owner1.getPets().add(puneetPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress(" Test address of owner2");
+        owner2.setCity("Redmond");
+        owner2.setTelephone("123123123");
+
+        Pet fionaCat = new Pet();
+        fionaCat.setName("Just Cat");
+        fionaCat.setOwner(owner2);
+        fionaCat.setBirthDate(LocalDate.now());
+        fionaCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionaCat);
 
         ownerService.save(owner2);
 
